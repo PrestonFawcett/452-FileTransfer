bbfd5af743307598d812b5300f004cf95b528f08
Benjamin-Heng <73806599+Benjamin-Heng@users.noreply.github.com>
2022-05-22 15:31:20 -0700

    experiment with hand generation on client side

--- client.py
+++ client.py
@@ -1,7 +1,21 @@
 import socket
+import random
 ClientMultiSocket = socket.socket()
 host = '127.0.0.1'
 port = 2004
+pnum1 = 0
+pnum2 = 0
+pnum3 = 0
+phand = []
+numberoptions = list(range(1, 13 + 3))
+pnum1 = random.choice(numberoptions)
+pnum2 = random.choice(numberoptions)
+pnum3 = random.choice(numberoptions)
+
+phand.append(pnum1)
+phand.append(pnum2)
+phand.append(pnum3)
+
 print('Waiting for Poker Server to implement')
 try:
     ClientMultiSocket.connect((host, port))
@@ -9,8 +23,14 @@ except socket.error as e:
     print(str(e))
 res = ClientMultiSocket.recv(1024)
 while True:
-    Input = input('Enter a number between 1-15: ')
-    ClientMultiSocket.send(str.encode(Input))
-    res = ClientMultiSocket.recv(1024)
-    print(res.decode('utf-8'))
+    print("Your hand:", phand)
+    Input = input('Enter a card to play: ')
+
+    if int(Input) not in phand:
+        print("invalid selection")
+    else:
+        phand.remove(int(Input))
+        ClientMultiSocket.send(str.encode(Input))
+        res = ClientMultiSocket.recv(1024)
+        print(res.decode('utf-8'))
 ClientMultiSocket.close()
